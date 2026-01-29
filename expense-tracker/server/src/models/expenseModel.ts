1a67775baacfd67ad81946e1fc806558b584d7f9
afza1 <afzalali5256@gmail.com>
2026-01-29 15:27:12 +0530

    typescript compatible expenseModel.ts

---
+++ expense-tracker/server/src/models/expenseModel.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document } from "mongoose";
+import { Schema } from "mongoose"; 
+
+export type ExpenseCategory =  | "Groceries" | "Liesure" | "Subscriptions" | "Dine out" | "Others"
+
+export interface IExpense {
+    userId: mongoose.Schema.Types.ObjectId,
+    amount: number,
+    category: ExpenseCategory,
+    description: string,
+    date: Date
+
+}
+
+const expenseSchema = new Schema<IExpense>({
+    userId: {type: mongoose.Types.ObjectId, ref: "User", required: true },
+    amount: {type: Number, required: true},
+    category: {type: String, enum: [ "Groceries" , "Liesure" ,"Subscriptions", "Dine out" , "Others"], required: true},
+    description: {type: String, required: true},
+    date: { type: Date, required: true}
+}, {timestamps: true})
+
+const  expenseModel = mongoose.model<IExpense>("Expense", expenseSchema);
+
+export { expenseModel };
