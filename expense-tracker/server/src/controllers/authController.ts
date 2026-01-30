b86819b72029128dffdf969d37f5f154a634862b
afza1 <afzalali5256@gmail.com>
2026-01-30 11:39:56 +0530

    p[]

---
+++ expense-tracker/server/src/controllers/authController.ts
@@ -0,0 +1,86 @@
+import type { Request, Response } from "express";
+import { userModel } from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+async function signup(req:Request, res:Response){
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!name || !email || !password) {
+      return res.status(404).json({
+        message: "All fields required",
+      });
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await userModel.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+    return res.status(200).json({
+      message: "User created succesfully",
+      user: user,
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+    }
+  }
+};
+
+async function signin(req:Request,res:Response){
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res.status(400).json({
+          message: "All fields required",
+        });
+      }
+  
+      const foundUser = await userModel.findOne({ email });
+      if (!foundUser) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+  
+      const passwordMatch = await bcrypt.compare(
+        password,
+        foundUser.password
+      );
+  
+      if (!passwordMatch) {
+        return res.status(401).json({
+          message: "Invalid credentials",
+        });
+      }
+  
+      const token = jwt.sign(
+        {
+          userId: foundUser._id,
+          email: foundUser.email,
+        },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "7d",
+        }
+      );
+  
+      return res.status(200).json({
+        message: "Signin successful",
+        token,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  };
+export { signin, signup }  ;
---
+++ expense-tracker/server/src/controllers/expenseController.ts
@@ -0,0 +1,108 @@
+import type { Request, Response } from "express";
+import { expenseModel } from "../models/expenseModel.js";
+import mongoose from "mongoose";
+
+
+//getExpense takes filter (filtering dates by days or months)
+//startdate, enddate and userId will in in params
+
+export const getExpenses = async (req: Request, res: Response) => {
+  const { filter, startDate, endDate } = req.query;
+  const userId = new mongoose.Types.ObjectId(req.user!.userId);
+
+  const query: any = { userId }; //query will be specific distinct userId
+  const now = new Date();
+  let start: Date | undefined
+
+
+  if (filter === "week") {
+    start = new Date();
+    start.setDate(now.getDate() - 7);
+  } else if (filter === "month") {
+    start = new Date();
+    start.setMonth(now.getMonth() - 1);
+  } else if (filter === "3months") {
+    start = new Date();
+    start.setMonth(now.getMonth() - 3);
+  } else if (startDate && endDate) {
+    query.date = {
+      $gte: new Date(startDate as string),
+      $lte: new Date(endDate as string),
+    };
+  }
+
+  if (start) {
+    query.date = { $gte: start };
+  }
+
+  const expenses = await expenseModel.find(query).sort({ date: -1 });
+  res.json(expenses);
+};
+
+export const addExpense = async (req: Request, res: Response) => {
+  const expense = await expenseModel.create({
+    ...req.body,
+    userId: new mongoose.Types.ObjectId(req.user!.userId),
+  });
+  res.status(201).json(expense);
+};
+
+export const updateExpense = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  
+  if (typeof id !== "string") {
+    return res.status(400).json({ message: "Invalid expense id" });
+  }
+
+  let expenseId: mongoose.Types.ObjectId;
+  try {
+    expenseId = new mongoose.Types.ObjectId(id);
+  } catch {
+    return res.status(400).json({ message: "Invalid expense id" });
+  }
+
+  const filter: any = { 
+    _id: expenseId, 
+    userId: req.user!.userId 
+  };
+
+  const expense = await expenseModel.findOneAndUpdate(
+    filter,
+    req.body,
+    { new: true }
+  );
+
+  if (!expense) {
+    return res.status(404).json({ message: "Expense not found" });
+  }
+
+  res.json(expense);
+};
+
+export const deleteExpense = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  
+  if (typeof id !== "string") {
+    return res.status(400).json({ message: "Invalid expense id" });
+  }
+
+  let expenseId: mongoose.Types.ObjectId;
+  try {
+    expenseId = new mongoose.Types.ObjectId(id);
+  } catch {
+    return res.status(400).json({ message: "Invalid expense id" });
+  }
+
+  const filter: any = {
+    _id: expenseId,
+    userId: req.user!.userId,
+  };
+
+  const deleted = await expenseModel.findOneAndDelete(filter);
+
+  if (!deleted) {
+    return res.status(404).json({ message: "Expense not found" });
+  }
+
+  res.json({ message: "Expense deleted" });
+};
